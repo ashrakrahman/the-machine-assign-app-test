045e9542c48e617ee40e3a05529234686ea73d44
ashrakrahman <lipu@mpower-social.com>
2020-06-30 13:50:42 +0600

    Updates

--- precache-manifest.ae0ca958eebffdac74ac8dba1b2dc847.js
+++ precache-manifest.ae0ca958eebffdac74ac8dba1b2dc847.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6b49ea2e40b869ccc0fb54e1fe5ed8b5",
+    "revision": "32ddcd38c8e5c814ae6e795fe3d1638c",
     "url": "/the-machine-assign-app-test/index.html"
   },
   {
-    "revision": "fb31f64904c89f535ae6",
+    "revision": "4b83c5de3d0a54ebb005",
     "url": "/the-machine-assign-app-test/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "229afe7e4a8ef04fd0f6",
+    "revision": "ac6fa5c9aeaf0eaf519b",
     "url": "/the-machine-assign-app-test/static/css/main.a865722b.chunk.css"
   },
   {
-    "revision": "fb31f64904c89f535ae6",
-    "url": "/the-machine-assign-app-test/static/js/2.c7452b45.chunk.js"
+    "revision": "4b83c5de3d0a54ebb005",
+    "url": "/the-machine-assign-app-test/static/js/2.88e00fe4.chunk.js"
   },
   {
     "revision": "67cd7a86caa59c4b38043aba94323370",
-    "url": "/the-machine-assign-app-test/static/js/2.c7452b45.chunk.js.LICENSE.txt"
+    "url": "/the-machine-assign-app-test/static/js/2.88e00fe4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "229afe7e4a8ef04fd0f6",
-    "url": "/the-machine-assign-app-test/static/js/main.cb98e039.chunk.js"
+    "revision": "ac6fa5c9aeaf0eaf519b",
+    "url": "/the-machine-assign-app-test/static/js/main.a7d0c57d.chunk.js"
   },
   {
     "revision": "57c2d329dd6a4143172f",
